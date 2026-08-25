858ac639b0bb6238a5a5d8dcb2976f4179393ec2
Adi <adi.shamay98@gmail.com>
2026-08-26 02:47:40 +0300

    delete last updated from json file

--- src/db.js
+++ src/db.js
@@ -15,7 +15,7 @@ async function getExchangeRates() {
                 return await response.json();
             }
         } catch (e) {
-            console.error("Failed to fetch rates from the user URL, trying default");
+            console.error("The real error is:", e);
         }
     }
 
